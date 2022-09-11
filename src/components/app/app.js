afeb371959fa1d17331659669a68440e8e41172f
Denis <denisgreshnaykov@gmail.com>
2022-09-11 22:40:06 +0500

    add delete item and add item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -9,23 +9,72 @@ import PostAddForm from "../post-add-form/post-add-form";
 import "./app.css";
 import "./index.css";
 
-const App = () => {
-  const data = [
-    { label: "Going to learn React", important: true, id: "sdf" },
-    { label: "That is so good", important: false, id: "swerwef" },
-    { label: "I need a break...", important: false, id: "sdqqwwef" },
-  ];
-  return (
-    <div className="app">
-      <AppHeader></AppHeader>
-      <div className="search-panel d-flex">
-        <SearchPanel></SearchPanel>
-        <PostStatusFilter></PostStatusFilter>
-      </div>
-      <PostList posts={data}></PostList>
-      <PostAddForm></PostAddForm>
-    </div>
-  );
-};
+import styled from "styled-components";
 
-export default App;
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
+
+// const StyledAppBlock = styled(AppBlock)`
+//   background-color: grey;
+// `;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { label: "Going to learn React", important: true, id: 1 },
+        { label: "That is so good", important: false, id: 2 },
+        { label: "I need a break...", important: false, id: 3 },
+      ],
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+    this.maxId = 4;
+  }
+
+  deleteItem(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+
+      const newArr = [...before, ...after];
+
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  addItem(body) {
+    const newItem = {
+      label: body,
+      important: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <AppBlock>
+        <AppHeader></AppHeader>
+        <div className="search-panel d-flex">
+          <SearchPanel></SearchPanel>
+          <PostStatusFilter></PostStatusFilter>
+        </div>
+        <PostList posts={this.state.data} onDelete={this.deleteItem}></PostList>
+        <PostAddForm onAdd={this.addItem}></PostAddForm>
+      </AppBlock>
+    );
+  }
+}
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,19 +1,23 @@
 import React from "react";
 
 import PostListItem from "../post-list-item";
+import { ListGroup } from "reactstrap";
 
 import "./post-list.css";
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, onDelete }) => {
   const elements = posts.map((item) => {
     const { id, ...itemProps } = item;
     return (
       <li key={id} className="list-group-item">
-        <PostListItem {...itemProps}></PostListItem>
+        <PostListItem
+          {...itemProps}
+          onDelete={() => onDelete(id)}
+        ></PostListItem>
       </li>
     );
   });
-  return <ul className="app-list list-group">{elements}</ul>;
+  return <ListGroup className="app-list">{elements}</ListGroup>;
 };
 
 export default PostList;
